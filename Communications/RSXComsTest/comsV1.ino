46cf3e901b34cc11575b21c9e150c13723364444
Aram Aprahamian <aram@apra.dev>
2024-03-17 13:39:38 -0700

    Multi node functionality works. Requires reflashing modems to 2.x

--- Communications/RSXComsTest/comsV1.ino
+++ Communications/RSXComsTest/comsV1.ino
@@ -1,7 +1,6 @@
-const int BAUD = 19200;
-const int CONSOLE_BAUD = 9600;
+const int BAUD = 57600;
+const int CONSOLE_BAUD = 57600;
 const String ID = "1";
-const int WRITERATE = 1; //How often this writes to radio per second
 
 long lastSend = millis();
 String recievedMessage;
@@ -13,10 +12,23 @@ void setup() {
   Serial1.begin(BAUD);
 
   Serial.println("Starting");
+
+  Serial1.println("1 Transmitted");
+  // Serial1.
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  // if(millis() - lastSend > 1000/WRITERATE) {
+  //   Serial.println("SENT: " + ID);
+  //   Serial1.println(ID.c_str());
+  //   // Serial1.println(ID);
+  //   lastSend = millis();
+
+  // }
+
+
   while(Serial1.available() > 0) {
     char out = Serial1.read();
     if (out == '\n') {
@@ -30,16 +42,24 @@ void loop() {
   if(Serial.available() > 0) {
     char input = Serial.read();
     if(input == '\n') {
-      Serial1.println(sendMessage);
+      Serial1.print(ID.c_str());
+      Serial1.println(": " + sendMessage);
+
+      Serial.print(ID.c_str());
+      Serial.println(": " + sendMessage);
+
       sendMessage = "";
     } else {
       sendMessage += input;
     }
   }
 
-  // if(millis() - lastSend > 1000/WRITERATE) {
-  //   Serial1.println(ID);
-  //   lastSend = millis();
+  // int lastRead = 0;
+  // while(Serial1.available() > 0) {
+  //   lastRead = Serial1.read();
+  //   char out = (char)lastRead;
+  //   Serial.print("Recieved: " + Serial1.readString());
   // }
+
 }
 
